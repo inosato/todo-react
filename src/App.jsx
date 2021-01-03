a36886d270aedb88d9d9ff2e8769e8c63c549e63
inosato <si17_21@yahoo.co.jp>
2021-01-03 14:04:29 +0900

    disabled

--- src/App.jsx
+++ src/App.jsx
@@ -42,7 +42,11 @@ const App = () => {
         todoText={todoText}
         onChange={onChangeTodoText}
         onClick={onClickAdd}
+        disabled={incompleteTodos.length >= 5}
       />
+      {incompleteTodos.length >= 5 && (
+        <p style={{ color: "red" }}>登録できるtodoは5個で</p>
+      )}
       <IncompleteTodos
         todos={incompleteTodos}
         onClickComplete={onClickComplete}
